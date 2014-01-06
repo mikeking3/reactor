b88d051a867797e5c5bdf3dc17069612d31b6824
Francisco McGee <franciscomcgee@gmail.com>
2014-01-05 22:35:40 -0800

    app.js should make some janky graphs

--- src/rasman/static/js/app.js
+++ src/rasman/static/js/app.js
@@ -24,5 +24,80 @@ var getCSV = $.get('/api/history/', function processCSV(data){
     };
     console.log(allHistory);
 });
+/*
+function csvify_1stream(timestamp_array, data_array, labels_array) {
+    var csvified;
+
+    // += the elements from timestamp_array and data_array into csvified
+    // csvified will not have any labels yet though
+    for (var i = 0; i < timestamp_array.length; i++)
+    {
+        csvified += timestamp_array[i] + "," + data_array[i] + "\n";
+    }
+    var csvified_labels =  labels_array.toString();
+    csvified_labels.append("\n");
+    csvified.prepend(csvified_labels);
+    return csvified;
+}
+*/
+composite_graph = new Dygraph(document.getElementById("composite_chart"), "/api/history/", {
+    title: 'Composite History',
+    width: 560,
+    stackedGraph: false,
+    legend: 'always',
+    labelsDiv: 'legenddiv',
+    labelsSeparateLines: true,
+    visibility: [true, true, true, true, true]
+});
+
+light_graph = new Dygraph(document.getElementById("light_chart"), "/api/history/", {
+    title: 'Light History',
+    width: 560,
+    stackedGraph: false,
+    legend: 'always',
+  //  labelsDiv: 'legenddiv',
+    labelsSeparateLines: true,
+    visibility: [false, false, false, false, true]
+});
+
+co2_graph = new Dygraph(document.getElementById("co2_chart"), "/api/history/", {
+    title: 'CO2 History',
+    width: 560,
+    stackedGraph: false,
+    legend: 'always',
+  //  labelsDiv: 'legenddiv',
+    labelsSeparateLines: true,
+    visibility: [false, true, false, false, false]
+});
+
+h2o_graph = new Dygraph(document.getElementById("h2o_chart"), "/api/history/", {
+    title: 'H2O History',
+    width: 560,
+    stackedGraph: false,
+    legend: 'always',
+  //  labelsDiv: 'legenddiv',
+    labelsSeparateLines: true,
+    visibility: [false, false, true, false, false]
+});
+
+ph_graph = new Dygraph(document.getElementById("ph_chart"), "/api/history/", {
+    title: 'pH History',
+    width: 560,
+    stackedGraph: false,
+    legend: 'always',
+  //  labelsDiv: 'legenddiv',
+    labelsSeparateLines: true,
+    visibility: [false, false, false, true, false]
+});
+
+temp_graph = new Dygraph(document.getElementById("temp_chart"), "/api/history/", {
+    title: 'Temp History',
+    width: 560,
+    stackedGraph: false,
+    legend: 'always',
+  //  labelsDiv: 'legenddiv',
+    labelsSeparateLines: true,
+    visibility: [true, false, false, false, false]
+});
 
 
